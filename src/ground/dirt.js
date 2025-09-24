bad120f00e3ffd62c9388c6be0e40b3b60bc122f
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-24 21:04:53 +0930

    Create dirt.js

---
+++ src/ground/dirt.js
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+import { resolveAssetUrl } from '../utils/asset-paths.js';
+
+export function createDirtGround({
+  size = 200,
+  repeat = 16,
+  height = 0,
+  receiveShadow = true,
+  anisotropy = 8,
+} = {}) {
+  const group = new THREE.Group();
+  group.name = 'ground:dirt';
+
+  const geo = new THREE.PlaneGeometry(size, size, 1, 1);
+  geo.rotateX(-Math.PI / 2);
+  geo.translate(0, height, 0);
+
+  const texLoader = new THREE.TextureLoader();
+
+  const color = texLoader.load(resolveAssetUrl('assets/textures/dirt/color.jpg'));
+  const rough  = texLoader.load(resolveAssetUrl('assets/textures/dirt/rough.jpg'));
+  const normal = texLoader.load(resolveAssetUrl('assets/textures/dirt/normal.jpg'));
+
+  [color, rough, normal].forEach(t => {
+    t.wrapS = t.wrapT = THREE.RepeatWrapping;
+    t.repeat.set(repeat, repeat);
+    t.anisotropy = Math.max(t.anisotropy || 0, anisotropy);
+  });
+
+  if ('SRGBColorSpace' in THREE) color.colorSpace = THREE.SRGBColorSpace;
+  else if ('sRGBEncoding' in THREE) color.encoding = THREE.sRGBEncoding;
+
+  const mat = new THREE.MeshStandardMaterial({
+    map: color,
+    roughnessMap: rough,
+    normalMap: normal,
+    roughness: 1.0,
+  });
+
+  const mesh = new THREE.Mesh(geo, mat);
+  mesh.receiveShadow = receiveShadow;
+
+  group.add(mesh);
+  return group;
+}
