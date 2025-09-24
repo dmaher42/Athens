778ce8a0f19419300c0348afb8933b251cec698b
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-24 18:26:15 +0930

    Update ground.js

--- src/scene/ground.js
+++ src/scene/ground.js
@@ -362,7 +362,7 @@ export async function loadGround(scene, renderer, options = {}) {
   if (grassTexture) {
     configureRepeatingTexture(grassTexture, TEXTURE_REPEAT, anisotropy, THREE.SRGBColorSpace);
     material.map = grassTexture;
-    material.color.set(0x00ff00);
+    material.color.set(0x4F8A10);
   }
 
   if (dustTexture) {
