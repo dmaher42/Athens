4538cdbca0a3fceb3c401aa74d514252a1947475
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-24 18:30:50 +0930

    Change ground material color from dark green to light green

--- src/scene/ground.js
+++ src/scene/ground.js
@@ -362,7 +362,7 @@ export async function loadGround(scene, renderer, options = {}) {
   if (grassTexture) {
     configureRepeatingTexture(grassTexture, TEXTURE_REPEAT, anisotropy, THREE.SRGBColorSpace);
     material.map = grassTexture;
-    material.color.set(0x4F8A10);
+    material.color.set(0x6DAA2C);
   }
 
   if (dustTexture) {
