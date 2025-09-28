788e2ac4cdfca9f5a6abf4274720bbd48ab7050e
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-28 22:02:51 +0930

    Remove stats.js dependency from debug shim

--- src/debug/statsShim.js
+++ src/debug/statsShim.js
@@ -1,20 +1,8 @@
 // Returns a Stats-like object with begin/end + optional DOM, or a no-op if stats.js isn’t available.
 export async function createStats() {
-  try {
-    const mod = await import('stats.js');
-    const Stats = mod.default || mod.Stats || window?.Stats;
-    if (!Stats) throw new Error('no Stats ctor');
-    const s = new Stats();
-    return {
-      dom: s.dom || s.domElement || null,
-      begin: () => s.begin?.(),
-      end: () => s.end?.()
-    };
-  } catch {
-    return {
-      dom: null,
-      begin: () => {},
-      end: () => {}
-    };
-  }
+  return {
+    dom: null,
+    begin: () => {},
+    end: () => {}
+  };
 }
